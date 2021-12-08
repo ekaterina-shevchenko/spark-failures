1d1285fb1d7b3c89da02102f6d3f4073f0abfdf3
Ekaterina Shevchenko <ekaterina.shevchenko@sap.com>
2021-12-08 11:02:46 +0100

    Created project structure and added README

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # spark-failures
-This project contains all code required for testing Spark under a set of state, network, and resource level failures
+This project contains all code required for testing Spark under a set of state, network, and resource level failures.
+
+The testing environment supports two ways of deploying, on-premises in a cluster of servers and in AWS. For instructions on how to deploy the project in each environment read the sections below.
+
+## AWS environment
+
+###Usage
+
+* Download [terraform](https://www.terraform.io/downloads.html) executable and add its location to PATH.
+* Provide AWS account credentials (in ~/.aws/credentials file or via environment variables)
+* Generate ssh-key (using `ssh-keygen` called **ec2/ec2.pub** and store it to **terraform/keypair** directory)
+* Navigate to **terraform/aws** directory and run the following commands from there. 
+* Initialize terraform (this needs to be run only once)
+```sh
+terraform init
+```
+* To build or update the infrastructure, run
+```sh
+terraform apply
+```  
+* When infrastructure is not required anymore, destroy it running
+```sh
+terraform destroy
+```
+
+###How it works
+
+1. Terraform connects to AWS using credentials provided in ~/.aws/credentials file or via environment variables and creates all required infrastructure (VPC, subnet, EC2 machines etc.) if it is not there.
+2. Upon creation of EC2 machines, terraform installs Docker on each machine and starts docker engine (see worker_userdata.sh)
+3. On the swarm manager node, terraform initializes swarm cluster and then uses generated token to add workers to the cluster, running a corresponding command on each worker (see manager_userdata.sh and worker_userdata.sh).
+4. Terraform creates docker overlay network.
+5. Once the swarm cluster is all set up, swarm manager creates all required containers using docker-compose.yml file (terraform runs the command). The current cluster topology is as follows:
+   * Node 1 (swarm manager) - Spark driver, Zookeeper (Spark HA mode)
+   * Node 2 (swarm worker) - Generator, Kafka
+   * Node 3 (swarm worker) - Spark master
+   * Node 4 (swarm worker) - Spark worker
+   * Node 5 (swarm worker) - Spark worker
+   * Node 6 (swarm worker) - Spark worker
+6. Upon creation, generator starts to generate event records and write them to a kafka topic. Driver creates a SparkContext, specifies operator topology and submits a job to Spark. This is run by swarm commands.
+
+**Warning**
+
+For testing purposes, terraform script creates public S3 buckets and places EC2 instances in a public subnet. Do not upload confidential data there, since it imposes security threats.
+
+
+## Local environment (on-premise)
+...
+
+###How it works
+
+1. Terraform connects to on-premise cluster machines, installs Docker on each machine and starts docker engine. All VMs should share the same network.
+2. On the swarm manager node, terraform initializes swarm cluster and then uses generated token to add workers to the cluster, running a corresponding command on each worker.
+4. Terraform creates docker overlay network.
+5. Once the swarm cluster is all set up, swarm manager creates all required containers using docker-compose.yml file (terraform runs the command). The current cluster topology is as follows:
+    * Node 1 (swarm manager) - Spark driver, Zookeeper (Spark HA mode)
+    * Node 2 (swarm worker) - Generator, Kafka
+    * Node 3 (swarm worker) - Spark master
+    * Node 4 (swarm worker) - Spark worker
+    * Node 5 (swarm worker) - Spark worker
+    * Node 6 (swarm worker) - Spark worker
+6. Upon creation, generator starts to generate event records and write them to a kafka topic. Driver creates a SparkContext, specifies operator topology and submits a job to Spark. This is run by swarm commands.
+
+## Monitoring
+...
+
+## Test suite
+...
+
+## Notes
+
+1. We should make sure that swarm load balancing will not impact distribution of events' records among Spark workers. It can be ensured via two ways:
+    * Create swarm workers as different resources (on contrary to one replicated resource). Then traffic will not be load-balanced.
+    * Make events' traffic going not through ingress network (load balancing is conducted by IPVS in a container created by docker swarm by default), which most probably will be the case since kafka and Spark workers will be deployed in the same overlay network.
---
+++ terraform/aws/main.tf
@@ -0,0 +1,235 @@
+# Configure the AWS provider
+provider "aws" {
+  region  = "us-east-1"
+  version = "~> 3.20.0"
+}
+
+# Create a VPC
+resource "aws_vpc" "vpc" {
+  cidr_block           = "192.1.0.0/16"
+  enable_dns_hostnames = "true"
+}
+
+# Attach IGW to the VPC
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+# Create a public subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "192.1.0.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = "true"
+}
+
+# Define a route table
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Assign the route table to the public subnet
+resource "aws_route_table_association" "web_public_rt_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+# Create an EC2 instance
+resource "aws_instance" "instance_1" {
+  ami                         = "ami-0be2609ba883822ec"
+  instance_type               = "t2.micro"
+  availability_zone           = "us-east-1a"
+  key_name                    = aws_key_pair.key_pair.key_name
+  associate_public_ip_address = "true"
+  vpc_security_group_ids      = [aws_security_group.sg_in.id, aws_security_group.sg_out.id]
+  subnet_id                   = aws_subnet.public_subnet.id
+  user_data                   = file("manager_userdata.sh")
+  private_ip                  = "192.1.0.101"
+  iam_instance_profile        = aws_iam_instance_profile.instance_profile.name
+}
+
+resource "aws_instance" "instance_2" {
+  ami                         = "ami-0be2609ba883822ec"
+  instance_type               = "t2.micro"
+  availability_zone           = "us-east-1a"
+  key_name                    = aws_key_pair.key_pair.key_name
+  associate_public_ip_address = "true"
+  vpc_security_group_ids      = [aws_security_group.sg_in.id, aws_security_group.sg_out.id]
+  subnet_id                   = aws_subnet.public_subnet.id
+  user_data                   = file("worker_userdata.sh")
+  private_ip                  = "192.1.0.102"
+  iam_instance_profile        = aws_iam_instance_profile.instance_profile.name
+}
+
+resource "aws_instance" "instance_3" {
+  ami                         = "ami-0be2609ba883822ec"
+  instance_type               = "t2.micro"
+  availability_zone           = "us-east-1a"
+  key_name                    = aws_key_pair.key_pair.key_name
+  associate_public_ip_address = "true"
+  vpc_security_group_ids      = [aws_security_group.sg_in.id, aws_security_group.sg_out.id]
+  subnet_id                   = aws_subnet.public_subnet.id
+  user_data                   = file("worker_userdata.sh")
+  private_ip                  = "192.1.0.103"
+  iam_instance_profile        = aws_iam_instance_profile.instance_profile.name
+}
+
+resource "aws_instance" "instance_4" {
+  ami                         = "ami-0be2609ba883822ec"
+  instance_type               = "t2.micro"
+  availability_zone           = "us-east-1a"
+  key_name                    = aws_key_pair.key_pair.key_name
+  associate_public_ip_address = "true"
+  vpc_security_group_ids      = [aws_security_group.sg_in.id, aws_security_group.sg_out.id]
+  subnet_id                   = aws_subnet.public_subnet.id
+  user_data                   = file("worker_userdata.sh")
+  private_ip                  = "192.1.0.104"
+  iam_instance_profile        = aws_iam_instance_profile.instance_profile.name
+}
+
+resource "aws_instance" "instance_5" {
+  ami                         = "ami-0be2609ba883822ec"
+  instance_type               = "t2.micro"
+  availability_zone           = "us-east-1a"
+  key_name                    = aws_key_pair.key_pair.key_name
+  associate_public_ip_address = "true"
+  vpc_security_group_ids      = [aws_security_group.sg_in.id, aws_security_group.sg_out.id]
+  subnet_id                   = aws_subnet.public_subnet.id
+  user_data                   = file("worker_userdata.sh")
+  private_ip                  = "192.1.0.105"
+  iam_instance_profile        = aws_iam_instance_profile.instance_profile.name
+}
+
+resource "aws_instance" "instance_6" {
+  ami                         = "ami-0be2609ba883822ec"
+  instance_type               = "t2.micro"
+  availability_zone           = "us-east-1a"
+  key_name                    = aws_key_pair.key_pair.key_name
+  associate_public_ip_address = "true"
+  vpc_security_group_ids      = [aws_security_group.sg_in.id, aws_security_group.sg_out.id]
+  subnet_id                   = aws_subnet.public_subnet.id
+  user_data                   = file("worker_userdata.sh")
+  private_ip                  = "192.1.0.106"
+  iam_instance_profile        = aws_iam_instance_profile.instance_profile.name
+}
+
+# Create S3 bucket with public reading access
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket        = "spark-failures-bucket"
+  acl           = "public-read"
+  force_destroy = "true"
+}
+
+# By default, AWS enables all four options when you create a new S3 bucket via
+# the AWS Management Console. However, you need to enable Block Public Access
+# explicitly when working with Terraform.
+# resource "aws_s3_bucket_public_access_block" "s3_bucket_pab" {
+#   bucket = aws_s3_bucket.s3_bucket.id
+#
+#   block_public_acls   = true
+#   block_public_policy = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
+
+# Upload source code to S3 bucket
+# resource "aws_s3_bucket_object" "source_code_upload" {
+#   bucket        = aws_s3_bucket.s3_bucket.id
+#   key           = "compiled_code_name.jar"
+#   source        = "${path.module}/../module_name/target/compiled_code_name.jar"
+#   force_destroy = true
+# }
+
+# Define security group allowing inbound public access
+resource "aws_security_group" "sg_in" {
+  name        = "security-group-all-in"
+  description = "Allow access to our infrastructure"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Define security group allowing outbound access to internet
+resource "aws_security_group" "sg_out" {
+  name        = "security-group-all-out"
+  description = "Allow access to internet"
+  vpc_id      = aws_vpc.vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Add key-pair
+resource "aws_key_pair" "key_pair" {
+  key_name   = "ec2"
+  public_key = file("keypair/ec2.pub")
+}
+
+# Create IAM profile role which grants access to S3 buckets
+resource "aws_iam_role" "iam_role" {
+  name                  = "iam_role"
+  path                  = "/"
+  force_detach_policies = true
+  assume_role_policy    = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+# Create IAM instance profile
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "instance_profile"
+  role = aws_iam_role.iam_role.id
+}
+
+# Create IAM role policy which grants access to S3 buckets
+resource "aws_iam_role_policy" "iam_role_policy" {
+  name   = "iam_role_policy"
+  role   = aws_iam_role.iam_role.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": ["s3:ListBucket"],
+      "Resource": ["arn:aws:s3:::url-shortener-instances-bucket-sources"]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:DeleteObject"
+      ],
+      "Resource": ["arn:aws:s3:::url-shortener-instances-bucket-sources/*"]
+    }
+  ]
+}
+EOF
+}
