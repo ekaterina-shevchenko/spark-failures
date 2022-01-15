44ce2475d2a6490f08173654b683da32a1494c3e
Ekaterina Shevchenko <ekaterina.shevchenko@sap.com>
2022-01-15 08:34:19 +0100

    Added one more role to docker swarm nodes

--- terraform/aws/manager_userdata.sh
+++ terraform/aws/manager_userdata.sh
@@ -12,13 +12,17 @@ sudo aws s3 cp token/token.txt s3://spark-failures-bucket/token.txt
 # wait for swarm workers to connect to the cluster
 while true; do nodes=$( docker node ls | awk 'NR > 1 {print $1}' | wc -l ); if ((nodes == 4)); then break; fi; sleep 2; done
 
-# label other swarm nodes as workers
-sudo docker node ls | grep -v 'Leader' | awk 'NR > 1 {print $1}' | while read line; do sudo docker node update --label-add role=worker $line; done
+# label other swarm nodes as workers except one
+sudo docker node ls | grep -v 'Leader' | awk 'NR > 2 {print $1}' | while read line; do sudo docker node update --label-add role=worker $line; done
 # can be checked via "sudo docker node ls -f "node.label=role=worker""
 
-# label current swarm node as swarm master
-sudo docker node ls | grep 'Leader' | awk '{print $1}' | while read line; do sudo docker node update --label-add role=master $line; done
+# label one swarm node as master
+sudo docker node ls | grep -v 'Leader' | awk 'NR == 2 {print $1}' | while read line; do sudo docker node update --label-add role=master $line; done
 # can be checked via "sudo docker node ls -f "node.label=role=master""
+
+# label current swarm node as swarm manager
+sudo docker node ls | grep 'Leader' | awk '{print $1}' | while read line; do sudo docker node update --label-add role=manager $line; done
+# can be checked via "sudo docker node ls -f "node.label=role=manager""
 
 # download docker-compose.yml file from s3 bucket (before that it should be uploaded there via terraform)
 sudo aws s3 cp s3://spark-failures-bucket/docker-compose.yml ./docker-compose.yml
