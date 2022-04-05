1ea21a478c562564887f3faf9a8099fb87dd5d00
Ekaterina Shevchenko <ek.r.shevchenko@gmail.com>
2022-04-05 20:53:30 +0200

    Deleted validator module as it became a part of the meter functionality

--- validator/src/main/java/config/KafkaConfig.java
+++
@@ -1,8 +0,0 @@
-package config;
-
-public class KafkaConfig {
-
-    public static final String TOPIC_PURCHASES = "purchases";
-    public static final String BOOTSTRAP_KAFKA_SERVER = "localhost:29092";
-    public static final String TOPIC_OUTPUT = "output";
-}
