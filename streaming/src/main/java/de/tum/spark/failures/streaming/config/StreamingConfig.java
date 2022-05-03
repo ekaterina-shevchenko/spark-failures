221f051b886c8756076b4c703a05b77af25a95a6
Ekaterina Shevchenko <ekaterina.shevchenko@sap.com>
2022-05-03 18:15:58 +0200

    Added spark driver host setting

--- streaming/src/main/java/de/tum/spark/failures/streaming/config/StreamingConfig.java
+++ streaming/src/main/java/de/tum/spark/failures/streaming/config/StreamingConfig.java
@@ -6,6 +6,7 @@ import org.apache.spark.streaming.Durations;
 public class StreamingConfig {
 
     public static final String SPARK_MASTER = "spark://spark-master:7077";
+    public static final String SPARK_DRIVER = "172.24.40.135";
     public static final Duration BATCH_DURATION = Durations.seconds(10);
 
 }
